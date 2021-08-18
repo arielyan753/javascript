ae6a9b86e1658d609356f48f4e8467c133946528
LionGod <LionGod@DESKTOP-7VHDOCI>
2021-08-18 19:30:57 +0300

    added files

---
+++ script.js
@@ -0,0 +1,5 @@
+function calculate() {
+    let first =parseInt(document.getElementById('num1').value) ;
+    let sec =parseInt(document.getElementById('num2').value) ;
+    document.getElementById('resulte').value = first+sec ;
+}
